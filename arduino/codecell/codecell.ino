24173c945f990d474e44f1f3f95aeb4ecc4aaaba
Francesco Di Maggio <francesco.dimaggio@outlook.com>
2026-02-18 21:10:05 +0100

    Update codecell.ino header with author, libraries, and hardware info

--- arduino/codecell/codecell.ino
+++ arduino/codecell/codecell.ino
@@ -1,6 +1,9 @@
 /*
  * CodeCell
- * ESP32-C3/C6 + Adafruit BNO085 + VCNL4040
+ * Francesco Di Maggio, 2026
+ * 
+ * Libraries: Adafruit BNO08x, OSC by Adrian Freed  ← install via Library Manager
+ * Hardware:  CodeCell (ESP32-C3 / ESP32-C6)
  */
 
 #include "src/main.h"
